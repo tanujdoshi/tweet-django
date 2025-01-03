a344a0662e74648f5bb0d92a717e54151635d502
tanujdoshi <tanujdoshi3920@gmail.com>
2025-01-03 18:53:52 -0500

    Added: Vue frontend

--- backend/authapp/urls.py
+++ backend/authapp/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('set-csrf-token/', views.set_csrf_token, name='set_csrf_token'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
-    path('register/', views.register, name='register'),
+    path('set-csrf-token', views.set_csrf_token, name='set_csrf_token'),
+    path('login', views.login_view, name='login'),
+    path('logout', views.logout_view, name='logout'),
+    path('register', views.register, name='register'),
 ]
---
+++ frontend/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+</script>
+
+<template>
+  <router-view />
+</template>
+
+<style scoped>
+/* You can add any global styles here */
+</style>
